6c982a341d87b1b2a3a0902d279eff7ac177127a
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-03-19 13:30:20 -0700

    route for get all cheeses complete

---
+++ api/controllers/cheeSwhiz.js
@@ -0,0 +1,25 @@
+const bodyParser = require('body-parser');
+
+function allCheese(req, res) {
+  const knex = require('../../knex.js');
+  return knex('cheeses')
+  .join('animals', 'animals.id', '=', 'cheeses.animal_id')
+  .join('firmness', 'firmness.id', '=', 'cheeses.firmness_id')
+  .select('cheeses.id', 'cheeses.name', 'animals.animal', 'firmness.firmness')
+  .orderBy('id', 'asc')
+  .then((cheeses) => {
+    res.set('Content-Type', 'application/json');
+    res.status(200).json(cheeses);
+  }).catch((err) => {
+    console.error(err);
+  });
+}
+
+function postCheese(req, res) {
+  const knex = require('../../knex.js')
+}
+
+module.exports = {
+  allCheese: allCheese,
+
+}
